e7affe993518b5219efddc5900c6bfb0866c7a31
maluhmc <maluhmc02@gmail.com>
2025-04-01 13:59:44 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
